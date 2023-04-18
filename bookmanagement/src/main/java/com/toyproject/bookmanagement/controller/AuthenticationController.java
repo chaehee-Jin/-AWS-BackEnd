a5c11a4be1f31a334b403e99f3502b8364f8c84b
heehee <heeheer_g@naver.com>
2023-04-18 17:42:30 +0900

    수정

--- bookmanagement/src/main/java/com/toyproject/bookmanagement/controller/AuthenticationController.java
+++ bookmanagement/src/main/java/com/toyproject/bookmanagement/controller/AuthenticationController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ public class AuthenticationController {
 	public ResponseEntity<?> login(){
 		return ResponseEntity.ok(null);
 	}
+	@CrossOrigin
 	@ValidAspect
 	@PostMapping("/signup")
 	public ResponseEntity<?>signup(@Valid @RequestBody SignupReqDto signupReqDto, BindingResult bindingResult){
