cd33e5ec6219ac7d4340225843c1a1fe5f5015f4
heehee <heeheer_g@naver.com>
2023-04-19 12:49:26 +0900

    정규식

--- bookmanagement/src/main/java/com/toyproject/bookmanagement/dto/auth/SignupReqDto.java
+++ bookmanagement/src/main/java/com/toyproject/bookmanagement/dto/auth/SignupReqDto.java
@@ -22,7 +22,7 @@ public class SignupReqDto {
 	@Email // 이메일 형식이 아니면 받아들이지 않음
 	private String email;
 
-	@Pattern(regexp = " ^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,16}$", message = "비밀번호는 영문자, 숫자, 특수문자를 포함하여 8~ 16자로 작성") // ->정규식
+	@Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,16}$", message = "비밀번호는 영문자, 숫자, 특수문자를 포함하여 8~ 16자로 작성") // ->정규식
 	private String passowrd;
 
 	@Pattern(regexp = "^[가-힣]{2,7}$", message="이름은 한글이름만 작성 가능합니다") // 한글외에는 받지 않겠다, 글자는 2~7글자까지 허용하겠다는 것을 의미
